b9c1ded6a346d0c2694d2b88673e38a0477fc918
BinaryBot3 <meerabakbar932@gmail.com>
2024-05-03 16:51:55 +0500

    first cpp program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double weight_kg;
+    double weight_pd;
+
+    cout << "Please enter your weight in kilograms : ";
+    cin >> weight_kg;
+
+    weight_pd = weight_kg * 2.2;
+    cout << "your weight in pounds is : " << weight_pd << endl;
+    return 0;
+}
